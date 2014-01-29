9dd5ab53fd1b2ec3746477965ca18049ccc3a9ba
catfriend <eakari@live.com>
2014-01-28 16:37:32 -0800

    first commit of Railscast 417 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZurbStore::Application.config.secret_key_base = '21322194a2243ce92f984be5b9c5f4667cc70740bbb47dc41a4471089012cab6a60665460f8c8983f0e16739cff693bd7e7550850298fc87344ad6f083eafb76'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ZurbStore::Application.config.session_store :cookie_store, key: '_zurb_store_session'
